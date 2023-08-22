b5c68652508f4faac62e0a26d8cf82194b5a28d2
tendenciasddddddddddddd <10004095632w@gmail.com>
2023-08-21 20:47:46 -0500

    AJUSTES DE REPORTES2

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ config();
 //mongodb://127.0.0.1/sistema-integrado2
 //mongodb+srv://steban:Medid100.@face-2.gkdmqm9.mongodb.net/sistema-uel?retryWrites=true&w=majority
 export default {
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb://127.0.0.1/sistema-integrado2",
+  MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://steban:Medid100.@face-2.gkdmqm9.mongodb.net/sistema-uel?retryWrites=true&w=majority",
   PORT: process.env.PORT || 3000,
   SECRET: 'system-integrado-unidad-edu-libertad'
 };
