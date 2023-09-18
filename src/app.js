17e6a6d4b5c5b7b434e00728b462839721f4dab9
tendenciasddddddddddddd <10004095632w@gmail.com>
2023-09-17 21:53:52 -0500

    UX PAGINA PUBLICA 23

--- src/app.js
+++ src/app.js
@@ -93,8 +93,8 @@ app.use(compression())
 //app.use('/uploads', express.static(__dirname +'../videos'));
 //app.use('/uploads', express.static(path.join(__dirname, '..', 'videos')))
 //app.use('/images', express.static(path.join(__dirname, '..', 'images')))
-app.use(express.static(__dirname + '/public'));
-//app.use('/assets', express.static(__dirname + '/public/assets'))
+//app.use(express.static(__dirname + '/public'));
+app.use('/assets', express.static(__dirname + '/public/assets'))
 app.use('/', publico);
 
 app.use("/api/auth", authRoutes);
