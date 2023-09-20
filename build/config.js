45d431e8f0ecc0ad9c77a24bde305ed196330ef6
tendenciasddddddddddddd <10004095632w@gmail.com>
2023-09-19 20:09:58 -0500

    first commit3

--- build/config.js
+++ build/config.js
@@ -8,10 +8,10 @@ exports.default = void 0;
 var _dotenv = require("dotenv");
 
 (0, _dotenv.config)(); //mongodb://127.0.0.1/sistema-symtech
-//mongodb+srv://steban:Medid100.@uecam.olnwazy.mongodb.net/sistema?retryWrites=true&w=majority
+//mongodb+srv://steban:Medid100.@face-1.ubvfiwd.mongodb.net/sistema-mons-tulcan?retryWrites=true&w=majority
 
 var _default = {
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://steban:Medid100.@uecam.olnwazy.mongodb.net/sistema?retryWrites=true&w=majority",
+  MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://steban:Medid100.@face-1.ubvfiwd.mongodb.net/sistema-mons-tulcan?retryWrites=true&w=majority",
   PORT: process.env.PORT || 5000,
   SECRET: 'system-unidad-educativa-leonidas-proaño.'
 };
--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ config();
 //mongodb://127.0.0.1/sistema-symtech
 //mongodb+srv://steban:Medid100.@face-1.ubvfiwd.mongodb.net/sistema-mons-tulcan?retryWrites=true&w=majority
 export default {
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb://127.0.0.1/sistema-symtech",
+  MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://steban:Medid100.@face-1.ubvfiwd.mongodb.net/sistema-mons-tulcan?retryWrites=true&w=majority",
   PORT: process.env.PORT || 5000,
   SECRET: 'system-unidad-educativa-leonidas-proaño.'
 };
