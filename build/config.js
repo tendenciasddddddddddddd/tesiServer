9604964b8d50746d7d38b88e67482c9df1a49160
tendenciasddddddddddddd <10004095632w@gmail.com>
2023-09-19 21:45:07 -0500

    first commit3336

--- build/config.js
+++ build/config.js
@@ -9,9 +9,10 @@ var _dotenv = require("dotenv");
 
 (0, _dotenv.config)(); //mongodb://127.0.0.1/sistema-symtech
 //mongodb+srv://steban:Medid100.@face-1.ubvfiwd.mongodb.net/sistema-mons-tulcan?retryWrites=true&w=majority
+//mongodb+srv://steban:Medid100.@pcei-tulcan.2xf8enw.mongodb.net/sistema-mons-tulcan?retryWrites=true&w=majority
 
 var _default = {
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://steban:Medid100.@face-1.ubvfiwd.mongodb.net/sistema-mons-tulcan?retryWrites=true&w=majority",
+  MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://steban:Medid100.@pcei-tulcan.2xf8enw.mongodb.net/sistema-mons-tulcan?retryWrites=true&w=majority",
   PORT: process.env.PORT || 5000,
   SECRET: 'system-unidad-educativa-leonidas-proaño.'
 };
